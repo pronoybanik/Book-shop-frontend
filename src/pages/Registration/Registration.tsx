e8128d44e47b483233a592625e89f389e9811b2a
Pronoy Banik <pronoybanik82@gmail.com>
2025-05-28 20:41:02 +0600

    chnage loading desgin

--- src/pages/Registration/Registration.tsx
+++ src/pages/Registration/Registration.tsx
@@ -161,10 +161,7 @@ const Registration = () => {
                   </h2>
                   <p className="mt-2 text-sm text-gray-600">
                     Already have an account?{" "}
-                    <Link
-                      to="/logIn"
-                      className="text-[#f96d6d]  font-medium"
-                    >
+                    <Link to="/logIn" className="text-[#f96d6d]  font-medium">
                       Sign in
                     </Link>
                   </p>
@@ -202,7 +199,7 @@ const Registration = () => {
                         id="name"
                         {...register("name", { required: "Name is required" })}
                         className="pl-10 block w-full rounded-lg border-gray-300 border py-3 px-4 focus:border-blue-500 focus:ring-blue-500 text-gray-900"
-                        placeholder="John Doe"
+                        placeholder="Name"
                       />
                     </div>
                     {errors.name && (
@@ -385,33 +382,35 @@ const Registration = () => {
                   {/* Submit Button */}
                   <div>
                     <PrimaryButton>
-                      {isLoading ? (
-                        <>
-                          <svg
-                            className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                          >
-                            <circle
-                              className="opacity-25"
-                              cx="12"
-                              cy="12"
-                              r="10"
-                              stroke="currentColor"
-                              strokeWidth="4"
-                            ></circle>
-                            <path
-                              className="opacity-75"
-                              fill="currentColor"
-                              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                            ></path>
-                          </svg>
-                          Creating Account...
-                        </>
-                      ) : (
-                        "Create Account"
-                      )}
+                      <div className="flex items-center justify-center w-full">
+                        {isLoading ? (
+                          <>
+                            <svg
+                              className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                            >
+                              <circle
+                                className="opacity-25"
+                                cx="12"
+                                cy="12"
+                                r="10"
+                                stroke="currentColor"
+                                strokeWidth="4"
+                              ></circle>
+                              <path
+                                className="opacity-75"
+                                fill="currentColor"
+                                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                              ></path>
+                            </svg>
+                            Creating Account...
+                          </>
+                        ) : (
+                          "Create Account"
+                        )}
+                      </div>
                     </PrimaryButton>
                   </div>
                 </form>
