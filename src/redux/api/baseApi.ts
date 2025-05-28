1840eb169f4be76ae54b8d82b8604f89776dd411
Pronoy Banik <pronoybanik82@gmail.com>
2025-05-28 20:24:05 +0600

    setup live link

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -2,8 +2,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseApi = createApi({
     reducerPath: "baseApi",
-    // baseQuery: fetchBaseQuery({ baseUrl: "https://book-shop-serversite.vercel.app/api" }),
-    baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3000/api" }),
+    baseQuery: fetchBaseQuery({ baseUrl: "https://book-shop-serversite.vercel.app/api" }),
+    // baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3000/api" }),
     tagTypes: ["books", "user", "order", "blog"],
     endpoints: () => ({}),
 });
