2b517f24ae48da378c323d6ec5c3ebec91ac5520
Pronoy Banik <pronoybanik82@gmail.com>
2025-12-31 19:10:42 +0600

    add new category

--- src/pages/CreateBook/CreateBook.tsx
+++ src/pages/CreateBook/CreateBook.tsx
@@ -158,6 +158,21 @@ const CreateBook = () => {
               <option value="SelfDevelopment">Self-Development</option>
               <option value="Poetry">Poetry</option>
               <option value="Religious">Religious</option>
+              <option value="Horror">Horror</option>
+              <option value="Comedy">Comedy</option>
+              <option value="Thriller">Thriller</option>
+              <option value="Mystery">Mystery</option>
+              <option value="Fantasy">Fantasy</option>
+              <option value="Romance">Romance</option>
+              <option value="Adventure">Adventure</option>
+              <option value="Biography">Biography</option>
+              <option value="History">History</option>
+              <option value="Philosophy">Philosophy</option>
+              <option value="Psychology">Psychology</option>
+              <option value="Children">Children</option>
+              <option value="YoungAdult">Young Adult</option>
+              <option value="Crime">Crime</option>
+              <option value="Drama">Drama</option>
             </select>
             {errors.category && (
               <p className="text-red-500 text-sm">{errors.category.message}</p>
